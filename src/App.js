d43b14d2fa31cdfc427949c9f4d4c7944402df5b
nerd35 <umukorochinedu@gmail.com>
2020-05-31 01:52:37 +0100

    social media authentication with create-react-app and firebase. users would be able to login with facebook, gmail, github, email, phone number

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import firebase from 'firebase'
+
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth'
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyCVYl18lnDrbW-mqMvCx8FJ0QnP9kjbSk4',
+  authDomain: 'fir-auth-5ee50.firebaseapp.com'
+})
+class App extends Component {
+  state = { isSignedIn: false }
+  uiConfig = {
+    signInFlow: 'popup',
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      firebase.auth.GithubAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      firebase.auth.PhoneAuthProvider.PROVIDER_ID
+    ],
+    callbacks: {
+      signInSuccess: () => false
+    }
+  }
+
+  componentDidMount = () => {
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({ isSignedIn: !!user })
+      console.log('user', user)
+    })
+  }
+  render () {
+    return (
+      <div className='App'>
+        {this.state.isSignedIn ? (
+          <div>
+            <h1>Signed In!!</h1>
+            <button
+              style={{
+                backgroundColor: 'blue',
+                padding: '1rem 2rem',
+                color: 'white',
+                border: 'none'
+              }}
+              onClick={() => firebase.auth().signOut()}
+            >
+              Sign Out
+            </button>
+            <h1>Welcome {firebase.auth().currentUser.displayName}</h1>
+            <img
+              src={firebase.auth().currentUser.photoURL}
+              alt='profilePicture'
+            />
+          </div>
+        ) : (
+          <StyledFirebaseAuth
+            uiConfig={this.uiConfig}
+            firebaseAuth={firebase.auth()}
+          />
+        )}
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
